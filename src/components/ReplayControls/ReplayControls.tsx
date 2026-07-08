232de8e71d3ad0002cb47dcef16e9bdc3f5f0873
Volodymyr Buhlak <suedanny2@yahoo.com>
2026-07-08 20:25:00 +0300

    Added drag control and arrow keys control for replay controller so now it has 3 ways to move replay time

--- src/components/ReplayControls/ReplayControls.tsx
+++ src/components/ReplayControls/ReplayControls.tsx
@@ -3,7 +3,7 @@
 import { useReplayStore } from "@/store/replayStore";
 import styles from "./ReplayControls.module.css";
 import { formatTime } from "@/lib/format";
-import { useRef } from "react";
+import { useRef, type ReactEventHandler } from "react";
 
 const SPEEDS = [1, 10, 30] as const;
 
@@ -16,8 +16,13 @@ export default function ReplayControls() {
   const cursor = useReplayStore(s => s.cursor)
   const duration = useReplayStore(s => s.duration)
   const seek = useReplayStore(s => s.seek)
-  const progress = (cursor / duration)*100
-  // const isDraggingRef = useRef(false)
+  const progress = duration > 0 ? (cursor / duration)*100 : 0
+  const isDraggingRef = useRef(false)
+  function seekFromPointer(e: React.MouseEvent<HTMLDivElement>) {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const sub = (e.clientX - rect.left) / rect.width
+    seek(sub*duration)
+  }
   return (
     <div className={`card ${styles.bar}`}>
       <button className={styles.play} aria-label={isPlaying ? 'Зупинити' : "Відтворити"} onClick={isPlaying ? pause : play}>
@@ -26,9 +31,28 @@ export default function ReplayControls() {
       <div className={styles.track} role="slider" aria-label="Позиція реплею"
           aria-valuemin={0} aria-valuemax={duration} aria-valuenow={cursor} tabIndex={0}
           onClick={e => {
-            const rect = e.currentTarget.getBoundingClientRect();
-            const sub = (e.clientX - rect.left) / rect.width
-            seek(sub*duration)
+            seekFromPointer(e)
+          }}
+          onPointerDown={e => {e.currentTarget.setPointerCapture(e.pointerId)
+            isDraggingRef.current = true
+          }}
+          onPointerUp={e => {
+            e.currentTarget.releasePointerCapture(e.pointerId)
+            isDraggingRef.current = false
+          }}
+          onPointerMove={e => {
+            if (!isDraggingRef.current) return
+            seekFromPointer(e)
+          }}
+          onKeyDown={e => {
+            if (e.key === 'ArrowLeft'){
+              e.preventDefault()
+              seek(cursor - 10000)
+            }
+            if (e.key === 'ArrowRight') {
+              e.preventDefault()
+              seek(cursor + 10000)
+            }
           }}
           >
         <div className={styles.progress} style={{ width: `${progress}%` }} />
